d40d9dc5c2d8b58f6c886ea50c7c0e99e1ce3d62
神馬滉裕 <shahryar@jinbahiroshihiroshinoMacBook-Pro.local>
2025-12-23 11:42:40 +0900

    feat(schedule): implement schedule model and CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get "schedules/index"
-  get "schedule/index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,6 +11,6 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root 'schedules#index'
   resources :schedules
 end
